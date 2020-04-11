c661df263c097f7742fe4a528d5a248529310b8e
乔路非 <43025511+qiaolufei@users.noreply.github.com>
2020-04-11 11:25:13 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import elementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import $ from 'jquery'
 import axios from 'axios'
